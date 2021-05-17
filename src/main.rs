ded6ae837d846299ff6edb0d1a1302d67e68cec3
Michael Bikovitsky <biko@bikodbg.com>
2021-05-17 21:43:41 +0300

    Implement command-line parsing for the application

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ extern crate json;
 
 use std::error::Error;
 
+use clap::{crate_authors, crate_name, crate_version, App, Arg};
+
 use execution_logger::ExecutionLogger;
 use process::ProcessCreator;
 
@@ -15,11 +17,34 @@ mod process;
 mod util;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<_> = std::env::args().skip(1).collect();
+    let args = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("Creates compile_commands.json from uncooperating build systems")
+        .setting(clap::AppSettings::TrailingVarArg)
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .value_name("FILENAME")
+                .help("Compilation database path")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("COMMAND")
+                .required(true)
+                .multiple(true)
+                .help("Command to run"),
+        )
+        .get_matches();
+
+    let command: Vec<_> = args.values_of("COMMAND").unwrap().collect();
+
+    let output = args.value_of("output").unwrap_or("compile_commands.json");
 
     let mut logger = ExecutionLogger::new();
 
-    logger.log(&ProcessCreator::new_with_arguments(&args, false))?;
+    logger.log(&ProcessCreator::new_with_arguments(&command, false))?;
 
     Ok(())
 }
