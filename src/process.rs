ed21b79c8562ef4d750c9b6327660a1798358d35
Michael Bikovitsky <biko@bikodbg.com>
2021-05-22 10:26:56 +0300

    Add license notices to remaining files

--- src/process.rs
+++ src/process.rs
@@ -1,3 +1,24 @@
+/*
+   Copyright (C) 2021 by Michael Bikovitksy
+
+   This file is part of winbear.
+
+   winbear is a tool to generate a compilation database for clang tooling.
+
+   winbear is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   winbear is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with winbear.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 use std::{
     collections::HashMap, convert::TryInto, error::Error, ffi::OsString, fmt::Debug,
     os::windows::ffi::OsStringExt, path::PathBuf,
